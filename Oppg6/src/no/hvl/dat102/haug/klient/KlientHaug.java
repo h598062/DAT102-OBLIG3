563d7f4a5e893c410a644acf425b32e20e511cf3
Spirevipp <bjornar.helgeland@hotmail.com>
2023-03-28 22:52:20 +0200

    oppgave 6f

--- Oppg6/src/no/hvl/dat102/haug/klient/KlientHaug.java
+++ Oppg6/src/no/hvl/dat102/haug/klient/KlientHaug.java
@@ -6,18 +6,18 @@ public class KlientHaug {
 	public static void main(String[] a) {
 		// Tester haugen ved å sortere verdier i en tabell
 		int[] tab = {300, 10, 30, 2, 100, 33, 1, 200, 18, 54};
-
+		System.out.println("Rekkefølge på innlegging av tall:");
 		TabellHaug<Integer> h1 = new TabellHaug<>();
 		for (int i = 0; i < tab.length; i++) {
-
+			System.out.print(tab[i] + "  ");
 			h1.leggTilElement((tab[i]));
 		}
+		System.out.println();
 
-
-		System.out.println("Verdiene i tabellen er nå:");
+		System.out.println("\nVerdiene i tabellen er nå:");
 		h1.skrivTab();
 		// Tar ut av haugen og skriver i sortert rekkefølge
-		System.out.println("\nHaugen i sortert rekkefoelge:");
+		System.out.println("\nHaugen i sortert rekkefølge:");
 		while (!h1.erTom()) {
 			Integer h = h1.fjernMinste();
 			System.out.print(h + "  ");
--- Oppg6/src/no/hvl/dat102/haug/tabell/TabellHaug.java
+++ Oppg6/src/no/hvl/dat102/haug/tabell/TabellHaug.java
@@ -1,98 +1,167 @@
 package no.hvl.dat102.haug.tabell;
 
+/**
+ * Minimumshaug som holder enhver type element som utvider Comparable<br>
+ * Bruker at rot-noden starter ved indeks 1 i intern tabell
+ *
+ * @param <T> Typen element som haugen skal holde
+ */
 public class TabellHaug<T extends Comparable<T>> {
-	// Lager en minimumshaug
 
-	private T[] data;
+	/**
+	 * Tabell som holder data til haugen
+	 */
+	private T[] haug;
+
+	/**
+	 * Lagrer posisjon til siste brukte plass i tabellen til haugen
+	 */
 	private int antall;
 
 	private static final int STDK = 100;
 
 	public TabellHaug() {
-		data   = (T[]) new Comparable[STDK];
+		haug   = (T[]) new Comparable[STDK + 1];
 		antall = 0;
 	}
 
+	/**
+	 * Legger til et element i haugen
+	 *
+	 * @param el Elementet som skal legges til
+	 */
 	public void leggTilElement(T el) {
-		if (antall == data.length) {
+		if (antall + 1 == haug.length) {
 			utvidTabell();
 		}
-		data[antall] = el; // Plasser den nye helt sist
-		antall++;
-		if (antall > 1) {
-			reparerOpp(); // Bytt om oppover hvis nødvendig
+		int nyPos    = antall + 1;
+		int forelder = nyPos / 2;
+
+		while (forelder > 0 && el.compareTo(haug[forelder]) < 0) {
+			haug[nyPos] = haug[forelder];
+			nyPos       = forelder;
+			forelder    = nyPos / 2;
 		}
+		haug[nyPos] = el;
+		antall++;
 	}
 
+	/**
+	 * Dobler tabellen ved behov for utviding
+	 */
 	private void utvidTabell() {
-		// Dobler tabellen ved behov for utviding
-		int lengde = data.length;
+		int lengde = haug.length;
 		T[] ny     = (T[]) new Comparable[2 * lengde];
 		for (int i = 0; i < antall; i++) {
-			ny[i] = data[i];
+			ny[i] = haug[i];
 		}
-		data = ny;
+		haug = ny;
 	}
 
+	/**
+	 * Intern metode for å reparere haug når nye elementer legges til<br>
+	 * Ikke i bruk, integrert inn i leggTilElement() metoden
+	 */
 	private void reparerOpp() {
 		// Fyll ut
 	}
 
+	/**
+	 * Fjerner minste element fra haugen, som alltid skal være i rot-noden
+	 *
+	 * @return Minste element i haugen
+	 */
 	public T fjernMinste() {
 		T svar = null;
-		if (antall > 0) {
-			svar    = data[0];
-			data[0] = data[antall - 1];
-			reparerNed(); // Bytter om nedover hvis nødvendig
+		if (!erTom()) {
+			svar    = haug[1];
+			haug[1] = haug[antall];
 			antall--;
+			reparerNed();
 		}
+		//skrivTab();
 		return svar;
 	}
 
+	/**
+	 * Finner minste element i haugen, som skal være i rot-noden
+	 *
+	 * @return Minste element i haugen
+	 */
 	public T finnMinste() {
 		T svar = null;
-		if (antall > 0) {
-			svar = data[0];
+		if (!erTom()) {
+			svar = haug[1];
 		}
 		return svar;
 	}
 
+	/**
+	 * Intern metode for å reparere haugen ved fjerning av rot-node
+	 */
 	private void reparerNed() {
-		T hjelp;
-
 		boolean ferdig   = false;
-		int     forelder = 0; // Start i roten og sml med neste nivå
-		int     minbarn;
-		int     vbarn    = forelder * 2 + 1;
-		int     hbarn    = vbarn + 1;
-		while (!ferdig && vbarn < antall) { // Har flere noder lenger nede
-			minbarn = vbarn;
+		int     forelder = 1;
+		int     vbarn    = forelder * 2;
 
-			if ((hbarn < antall) && ((data[hbarn]).compareTo(data[vbarn]) < 0)) {
+		while (!ferdig && harVB(forelder)) { // Har flere noder lenger nede
+			int minbarn = vbarn;
+			int hbarn   = vbarn + 1;
+
+			if (harHB(forelder) && haug[hbarn].compareTo(haug[vbarn]) < 0) {
 				minbarn = hbarn;
 			}
 			// Har funnet det "minste" av barna. Sml med forelder
-			if ((data[forelder]).compareTo(data[minbarn]) <= 0) {
+			if (haug[forelder].compareTo(haug[minbarn]) <= 0) {
 				ferdig = true;
 			} else { // Bytt om og gå videre nedover hvis forelder er for stor
-				hjelp          = data[minbarn];
-				data[minbarn]  = data[forelder];
-				data[forelder] = hjelp;
-				forelder       = minbarn;
-				vbarn          = forelder * 2 + 1;
-				hbarn          = vbarn + 1;
+				T hjelp = haug[minbarn];
+				haug[minbarn]  = haug[forelder];
+				haug[forelder] = hjelp;
+
+				forelder = minbarn;
+				vbarn    = forelder * 2;
 			}
 		}
 	}
 
-	public boolean erTom() {
-		return antall == 0;
+	/**
+	 * Sjekker om en gitt nodeindeks har et venstrebarn
+	 *
+	 * @param node Noden en skal sjekke om har venstrebarn
+	 *
+	 * @return Sant om den gitte noden har et venstrebarn
+	 */
+	private boolean harVB(int node) {
+		return 2 * node <= antall;
 	}
 
+	/**
+	 * Sjekker om en gitt nodeindeks har et høyrebarn
+	 *
+	 * @param node Noden en skal sjekke om har høyrebarn
+	 *
+	 * @return Sant om den gitte noden har et høyrebarn
+	 */
+	private boolean harHB(int node) {
+		return 2 * node + 1 <= antall;
+	}
+
+	/**
+	 * Sjekker om haugen er tom
+	 *
+	 * @return Sant om haugen er tom
+	 */
+	public boolean erTom() {
+		return antall < 1;
+	}
+
+	/**
+	 * Hjelpemetode til test
+	 */
 	public void skrivTab() {
-		// Hjelpemetode til test
-		for (int i = 0; i < antall; i++) {
-			System.out.print(data[i] + " ");
+		for (int i = 1; i <= antall; i++) {
+			System.out.print(haug[i] + "  ");
 		}
 		System.out.println();
 	}
